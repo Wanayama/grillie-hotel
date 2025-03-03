54bf037a19b344cd0cb11a46b51c78be097ba160
Brian Wanyama <brianwanyama06@gmail.com>
2025-03-03 11:09:36 +0300

    final update

--- grillie-hotel-main/assets/js/script.js
+++ grillie-hotel-main/assets/js/script.js
@@ -168,3 +168,21 @@ window.addEventListener("mousemove", function (event) {
   }
 
 });
+
+
+//pop-ups
+function openPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function openPopup2() {
+  document.getElementById("popup2").style.display = "flex";
+}
+
+function closePopup2() {
+  document.getElementById("popup2").style.display = "none";
+}
